24cee81566391217f40811373a73fae06212f82e
Rex <Rex@GCMD-LT-CXRH4LG9PH.local>
2026-06-01 19:59:53 +0800

    Update redirect URI for GitHub Pages

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   clientId: "768e8544-9eab-40cb-940e-5ba478af614f",
   authority: "https://login.microsoftonline.com/consumers",
-  redirectUri: "http://localhost:5500",
+  redirectUri: window.location.origin + window.location.pathname,
   filePath: "Master.xlsx",
   sheetName: "Transaction",
   scopes: ["Files.Read"]
